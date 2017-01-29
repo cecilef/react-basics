678fd22624f6c000318594b597d0a5802851ffc0
Cécile <acecileferrier@gmail.com>
2017-01-29 19:30:35 +0000

    List created, need sprites and types to be fetched

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import { PokemonList } from './poke-list/poke-list';
-import $ from 'jquery';
 
 export default class App extends React.Component {
-
-	fetchPokemons() {
-		// fetch list of pokemon from poke-api
-	}
 
 	render() {
     return (
       <div>
         <h1>Test Pokemon</h1>
-        <PokemonList pokemons={[
-					{img: 'img1', name: 'Bulbizarre', type: 'Plante'},
-					{img: 'img2', name: 'Salamèche', type: 'Feu'},
-					{img: 'img3', name: 'Carapuce', type: 'Eau'}
-				]}/>
+        <PokemonList pokemons={this.props.list}/>
       </div>
     )
   }
--- src/poke-list/poke-list.jsx
+++ src/poke-list/poke-list.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { PokemonListItem } from './poke-list-item';
+import $ from 'jquery';
 
 export class PokemonList extends React.Component {
 
 	render() {
-		return (
-			<table>
-				{this.props.pokemons.map((pokemon) => {
-					return <PokemonListItem img={pokemon.img} name={pokemon.name} type={pokemon.type}/>
-				})}
-			</table>
-		)
-	}
+			return (
+				<table>
+					<tbody>
+						{this.props.pokemons[0].results.map((pokemon) => {
+							var key = Math.random() * (1 - 150) + 1;
+							return <PokemonListItem img="test" name={pokemon.name} type="type" key={"key" + key}/>
+						})}
+					</tbody>
+				</table>
+			)
+		}
 }
